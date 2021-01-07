f6d3c80af4a556811b4277d0a32a5ad0681098cd
Nico <nicolas.mengozzi@outlook.fr>
2021-01-07 16:03:03 +0100

    dependencies modification

--- covidipedia.connectors/src/Config.cs
+++ covidipedia.connectors/src/Config.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using System.IO;
 
--- covidipedia.connectors/src/Fetch.cs
+++ covidipedia.connectors/src/Fetch.cs
@@ -1,12 +1,7 @@
 using System;
 using System.IO;
 using System.Net;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace covidipedia.connectors
 {

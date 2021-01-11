92e8ee06e5215a22a3cb5c4a8a9c2be18e18bf16
JetRequin <clement.ducollet@isen.yncrea.fr>
2021-01-11 17:36:30 +0100

    Table crée dynamiquement

--- covidipedia.front/src/Pages/Private/Query.cshtml.cs
+++ covidipedia.front/src/Pages/Private/Query.cshtml.cs
@@ -6,14 +6,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace covidipedia.front.Pages {
-    public class QueryModel : PageModel {
+namespace covidipedia.front.Pages
+{
+    public class QueryModel : PageModel
+    {
         private readonly ILogger<QueryModel> _logger;
+        public class Result
+        {
+            public DateTime date;
+            public string place;
+            public string info;
+        }
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
 
-        public QueryModel(ILogger<QueryModel> logger) {
+        public QueryModel(ILogger<QueryModel> logger)
+        {
             _logger = logger;
         }
 
-        public void OnGet(){}
+        public void OnGet() { }
     }
 }

5f92ca3eb68c11b6637c573ab10ab14763c6e337
Nico <nicolas.mengozzi@outlook.fr>
2021-01-11 11:32:12 +0100

    Maquette de front pour index

---
+++ covidipedia.front/wwwroot/js/index.js
@@ -0,0 +1,17 @@
+document.getElementById('checkbox-sales-countries').onchange = function(){
+    var salesCountries = document.getElementsByClassName('sales-countries')[0];
+    if (this.checked) salesCountries.classList.remove('hide');
+    else salesCountries.classList.add('hide');
+  }
+
+document.getElementById('checkbox-discount-countries').onchange = function(){
+    var salesCountries = document.getElementsByClassName('discount-countries')[0];
+    if (this.checked) salesCountries.classList.remove('hide');
+    else salesCountries.classList.add('hide');
+  }
+
+document.getElementById('checkbox-profit-month').onchange = function(){
+    var salesCountries = document.getElementsByClassName('profit-month')[0];
+    if (this.checked) salesCountries.classList.remove('hide');
+    else salesCountries.classList.add('hide');
+    }

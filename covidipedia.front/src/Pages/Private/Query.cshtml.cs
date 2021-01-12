2b0c9c436761bec4001584eaf5a6ee521f4d0899
Nico <nicolas.mengozzi@outlook.fr>
2021-01-12 10:35:35 +0100

    Entity

--- covidipedia.front/src/Pages/Private/Query.cshtml.cs
+++ covidipedia.front/src/Pages/Private/Query.cshtml.cs
@@ -11,22 +11,19 @@ namespace covidipedia.front.Pages
     public class QueryModel : PageModel
     {
         private readonly ILogger<QueryModel> _logger;
-        public class Result
-        {
+        public class Result {
             public DateTime date;
             public string place;
             public string info;
         }
         private static Random random = new Random();
-        public static string RandomString(int length)
-        {
+        public static string RandomString(int length) {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
-        public QueryModel(ILogger<QueryModel> logger)
-        {
+        public QueryModel(ILogger<QueryModel> logger) {
             _logger = logger;
         }
 

11c409c8a99fb812b4ae3f73a485f7999fb1e58f
Ratchet83 <44296022+Ratchet83@users.noreply.github.com>
2021-01-29 16:52:55 +0100

    CRUD Debut ?

---
+++ covidipedia.front/Areas/Identity/Pages/Account/Admin.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+
+namespace covidipedia.front.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class AdminModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public AdminModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
--- covidipedia.front/src/Pages/Admin/Admin.cshtml.cs
+++ covidipedia.front/src/Pages/Admin/Admin.cshtml.cs
@@ -2,15 +2,18 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace covidipedia.front.Pages {
+namespace covidipedia.front.Pages
+{
     [Authorize(Roles = "Admin")]
-    public class AdminModel : PageModel {
+    public class AdminModel : PageModel
+    {
         private readonly ILogger<AdminModel> _logger;
 
-        public AdminModel(ILogger<AdminModel> logger) {
+        public AdminModel(ILogger<AdminModel> logger)
+        {
             _logger = logger;
         }
 
-        public void OnGet(){}
+        public void OnGet() { }
     }
 }
